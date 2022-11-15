f39c1213056470e40e8e6a6d22322e456a50e1ee
el.einad@gmail.com <el.einad@gmail.com>
2022-11-15 19:49:27 +0100

    simple blur

--- shaders/fragment_sh.glsl
+++ shaders/fragment_sh.glsl
@@ -8,6 +8,7 @@ precision mediump float;
 uniform sampler2D u_image;
 uniform float u_time;
 uniform float u_flip_y;
+uniform vec2 u_resolution;
 
 // the texCoords passed in from the vertex shader.
 in vec2 v_texCoord;
@@ -15,10 +16,40 @@ in vec2 v_texCoord;
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
+
 void main() {
-  vec4 tex = texture(u_image, v_texCoord);
+  vec3 col = vec3(0.0);
+  float offset = 1.0 / u_resolution.x;
+
+  vec2 offsets[9] = vec2[9](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );;  
+
+  float div = 16.;
+  float kernel[9] = float[9](
+    1.0 / div, 2.0 / div, 1.0 / div,
+    2.0 / div, 4.0 / div, 2.0 / div,
+    1.0 / div, 2.0 / div, 1.0 / div  
+);
+
   if (u_flip_y == 1.) {
-    tex.r = tex.r * 1.5 * abs(sin(u_time));
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++) {
+        sampleTex[i] = vec3(texture(u_image, v_texCoord + offsets[i]));
+    }
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
   }
-  outColor = tex;
+  else {
+    col = texture(u_image, v_texCoord).rgb;
+  }
+  outColor = vec4(col, 1.);
 }
--- src/texture-common.js
+++ src/texture-common.js
@@ -29,8 +29,8 @@ const textureData = (function () {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
         // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
         return texture;
     }
