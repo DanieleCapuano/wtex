8e4ce8a452b953530a0067bec1d05b8cda3c9194
el.einad@gmail.com <el.einad@gmail.com>
2022-11-18 09:14:48 +0100

    texture data from wbase

--- src/program-loop.js
+++ src/program-loop.js
@@ -1,4 +1,4 @@
-import { draw_shapes, textureData, resizeCanvasToDisplaySize } from 'wbase';
+import { draw_shapes, texture_data, resizeCanvasToDisplaySize } from 'wbase';
 
 export const render_loop = _render;
 export const program_loop_fn = _draw_fbos_textures.bind(null, _draw_main_texture);
@@ -66,7 +66,7 @@ function _draw_fbos_textures(next_fn, current_program, gl, opts) {
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         uniforms.u_resolution.set(gl, current_program, '2f', [gl.canvas.width, gl.canvas.height]);
 
-        textureData.draw_into_texture(gl, inputEl, gl.canvas.width, gl.canvas.height);
+        texture_data.draw_into_texture(gl, inputEl, gl.canvas.width, gl.canvas.height);
         image_drawn_in_texture = true;
         opts.input.should_update_texture = false;
     }
@@ -80,16 +80,16 @@ function _draw_fbos_textures(next_fn, current_program, gl, opts) {
     // loop through each effect we want to apply.
     for (var ii = framebuffers_offset; ii < framebuffers_n + framebuffers_offset; ++ii) {
         // Setup to draw into one of the framebuffers.
-        textureData.set_framebuffer(
+        texture_data.set_framebuffer(
             gl,
-            textureData.get_fbo(current_program, ii),
+            texture_data.get_fbo(current_program, ii),
             gl.canvas.width, gl.canvas.height
         );
 
         draw_shapes(gl);
 
         // for the next draw, use as input texture (associated to gl.TEXTURE0) the texture we just rendered to.
-        gl.bindTexture(gl.TEXTURE_2D, textureData.get_fbo_texture(current_program, ii));
+        gl.bindTexture(gl.TEXTURE_2D, texture_data.get_fbo_texture(current_program, ii));
     }
 
     next_fn(current_program, gl);
@@ -99,7 +99,7 @@ function _draw_main_texture(current_program, gl) {
     const { uniforms, program } = current_program;
 
     uniforms.u_flip_y && uniforms.u_flip_y.set(gl, program, '1f', -1);
-    textureData.set_framebuffer(gl, null, gl.canvas.width, gl.canvas.height);
+    texture_data.set_framebuffer(gl, null, gl.canvas.width, gl.canvas.height);
 
     draw_shapes(gl);
 }
